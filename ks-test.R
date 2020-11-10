97714738c8799b6c507d70cb4800a43969c5031c
Leo Zeitler <leozeitler95@gmail.com>
2020-11-10 12:31:02 +0100

    Update: Implemented flag to define normalisation method
    Use remap or center. If binned KS-test is used, the remap
    normalisation should be used

--- ks-test.R
+++ ks-test.R
@@ -1,5 +1,18 @@
 ###############################################################################
-# Load/install libraries and files
+# Normalisation functions
+###############################################################################
+
+centre <- function(data){
+  return((data - mean(data)) / sd(data))
+}
+
+remap <- function(data){
+  n_data <- data - min(data)
+  return(n_data / max(n_data))
+}
+
+###############################################################################
+# Command line argument parser
 ###############################################################################
 suppressMessages(library("optparse"))
 
@@ -11,11 +24,23 @@ option_list = list(
   make_option('--bin_size', type='numeric', default=0.1, 
               help='Bin size'),
   make_option('--install', action='store_true', default=FALSE, 
-              help='Install libraries if passed')
+              help='Install libraries if passed'),
+  make_option(c('-n', '--normalise'), type='character', default='remap', 
+              help='The normalisation method that should be used.
+              Possible options are: remap (default) and center.
+              remap remaps the all values between 0 and 1 with 0 the 
+              minimum and 1 the maximum 
+              (use this when using the KS-test with binning);
+              center centres the data to zero mean and a std of 1',
+              metavar='character')
 )
 
 opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
+
+###############################################################################
+# Load/install libraries and files
+###############################################################################
 
 if(opt$install == TRUE){
   source('http://www.bioconductor.org/biocLite.R')
@@ -31,20 +56,30 @@ suppressMessages(library('rtracklayer'))
 suppressMessages(library('devtools'))
 suppressMessages(library('ggplot2'))
 
+###############################################################################
+# Main
+###############################################################################
+
 org_df <- import.bw(opt$original_file)
 ref_df <- import.bw(opt$comparison_file)
 
 org_scores <- score(org_df)
 ref_scores <- score(ref_df)
 
-org_scores <- org_scores / max(org_scores)
-org_scores <- org_scores - min(org_scores)
-ref_scores <- ref_scores / max(ref_scores)
-ref_scores <- ref_scores - min(ref_scores)
+if(tolower(opt$normalise) == 'remap'){
+  print('############### Use remap normalisation')
+  org_scores <- remap(org_scores)
+  ref_scores <- remap(ref_scores)
+} else if(tolower(opt$normalise) == 'center'){
+   print('############### Use center normalisation')
+   org_scores <- centre(org_scores)
+   ref_scores <- centre(ref_scores)
+} else {
+  stop('Invalid normalisation method. Pass remap or center.')
+}
 
 print('############### KS test w/out binning')
 ks.test(org_scores, ref_scores)
-
 
 # KS based on previously calculated CDF
 uv4_cdf <- ecdf(org_scores)(seq(min(org_scores), max(org_scores), by=opt$bin_size))
@@ -68,4 +103,6 @@ plot(density(org_scores), col='green')
 lines(density(ref_scores), col='blue')
 
 print('############### KS test w/ binning')
+sprintf('############### Number of bins orginial data: %i', length(uv4_cdf))
+sprintf('############### Number of bins reference data: %i', length(uv3b_cdf))
 ks.test(uv4_cdf, uv3b_cdf)
